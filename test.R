e02306a24e20b2d1b2d226e1d793a7b6bd23f013
이기준 <2stndard@gmail.com>
2021-02-23 00:32:27 +0900

    210223-1

--- test.R
+++ test.R
@@ -690,3 +690,11 @@ ggplot(students, aes(x = 연도, y = 학생수계)) +
 
 ?as.Date
 ?read.csv                     
+
+
+holidays <- tourism %>%
+  filter(Purpose == "Holiday") %>%
+  group_by(State) %>%
+  summarise(Trips = sum(Trips))
+
+holidays %>% autoplot(Trips)
